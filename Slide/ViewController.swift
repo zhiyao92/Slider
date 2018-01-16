ce3e74df268b1c743976f0a84b305770fa2d76b5
Kelvin Tan <kelvintan@Kelvins-MacBook-Pro.local>
2018-01-16 10:30:45 +0800

    First Commit

--- Slide/ViewController.swift
+++ Slide/ViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var textValue: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func valueChanged(_ sender: Any) {
+        let currentValue = Int(slider.value)
+        textValue.text = "\(currentValue)"
     }
-
-
 }
 
